885a16550bbdf249c6c6b7774d727c0504719f9f
GitHub Action <action@github.com>
2022-04-14 09:17:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/kevinlinit/sturdyplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kevinlinit/sturdyplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kevinlinit.sturdyplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kevinlinit.sturdyplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
